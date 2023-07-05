8bfd179b49fad8b09cf0ad4388dcda8a7bf49485
destiny32601 <destiny32601@gmail.com>
2023-07-05 11:40:59 -0700

    fixed my issue and now the notes appear on the left side of the screen

--- routes/api.js
+++ routes/api.js
@@ -4,12 +4,12 @@ const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 
 router.get('/notes', async (req, res) => {
-    const dbJson = fs.readFileSync('/db/db.json', 'utf8');
-    res.json(dbJson);
+    const dbJson = fs.readFileSync('./db/db.json', 'utf8');
+    res.json(JSON.parse(dbJson));
 });
 
 router.post('/notes', (req, res) => {
-    const dbJson = JSON.parse(fs.readFileSync('/db/db.json', 'utf8'));
+    const dbJson = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
 
     const newNote = {
         id: uuidv4(),
@@ -18,7 +18,7 @@ router.post('/notes', (req, res) => {
     };
 
     dbJson.push(newNote);
-    fs.writeFileSync('/db/db.json', JSON.stringify(dbJson));
+    fs.writeFileSync('./db/db.json', JSON.stringify(dbJson));
     res.json(dbJson);
 });
 
